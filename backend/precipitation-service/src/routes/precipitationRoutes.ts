099bdf22808424d0b8192a90c915dcc5ed8f761b
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-12-19 17:12:09 +0100

    feat: init latest values path

--- backend/precipitation-service/src/routes/precipitationRoutes.ts
+++ backend/precipitation-service/src/routes/precipitationRoutes.ts
@@ -1,10 +1,13 @@
 import express from "express";
-import { controller } from "../controllers/precipitationController";
+import { getLatestX } from "../controllers/precipitationController";
 const API_TOKEN = "api";
 const version = "v0";
 const defaultPath = `/${API_TOKEN}/${version}`;
+const LATEST_VALUES = `/${defaultPath}/get-latest`
+const LATEST_X_VALUES = `/${LATEST_VALUES}/:x`
 
 
 const router = express.Router();
 
-router.route("")
+router.route(LATEST_X_VALUES)
+    .get(getLatestX)
