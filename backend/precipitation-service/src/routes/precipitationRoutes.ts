c85746e1dcf4821d958cbecfe18916a649988ba2
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-12-19 16:37:49 +0100

    feat: add API_TOKEN, version as constant values

--- backend/precipitation-service/src/routes/precipitationRoutes.ts
+++ backend/precipitation-service/src/routes/precipitationRoutes.ts
@@ -1,4 +1,10 @@
 import express from "express";
 import { controller } from "../controllers/precipitationController";
+const API_TOKEN = "api";
+const version = "v0";
+const defaultPath = `/${API_TOKEN}/${version}`;
+
 
 const router = express.Router();
+
+router.route("")
