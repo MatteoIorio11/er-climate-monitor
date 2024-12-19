0a1567230bd71e30ce577cb037689c6aaec7a7d1
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-12-19 17:28:30 +0100

    feat: add implementation for post handler

--- backend/precipitation-service/src/controllers/precipitationController.ts
+++ backend/precipitation-service/src/controllers/precipitationController.ts
@@ -1,12 +1,16 @@
 import { precipitationModel } from "../models/precipitationModel"
-import {Request, Response } from "express"
+import { Request, Response } from "express"
+
 
 const getLatestX = (request: Request, response: Response) => {
     response.send()
 }
 
 const postNewData = (request: Request, response: Response) => {
-    response.send()
+    const modelData = request.body;
+    const newData = new precipitationModel(modelData);
+    newData.save();
+    response.send(201);
 }
 
 export { getLatestX }
